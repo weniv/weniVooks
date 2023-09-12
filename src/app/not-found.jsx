d494e9486f9223290679912574766fdc63bb0940
dreamfulbud <dreamfulbud@naver.com>
2023-09-12 16:04:45 +0900

    💄 Style: 모바일 화면 스타일 수정

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -9,8 +9,8 @@ export default function NotFound() {
       <img src="/img/404.png" alt="404" />
       <h2>페이지를 찾을 수 없습니다.</h2>
       <p>
-        페이지가 존재하지 않거나 사용할 수 없는 페이지입니다. 웹 주소가 올바른지
-        확인해 주세요.
+        페이지가 존재하지 않거나 사용할 수 없는 페이지입니다.
+        <wbr /> 웹 주소가 올바른지 확인해 주세요.
       </p>
       <div className={styles.btnGroup}>
         <Button style="solid" href="./">
