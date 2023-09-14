f7606b7dc6a268185d681a43603e63df2e06dfa4
nurimeansworld <nurimeansworld@gmail.com>
2023-09-14 10:58:22 +0900

    :white_check_mark: Test: 노션 API 연동용 테스트 페이지 생성. 단순 내용 출력 설정 #4

---
+++ src/app/notionTest/page.js
@@ -0,0 +1,31 @@
+import { Client } from '@notionhq/client';
+
+async function getData() {
+  const notion = new Client({
+    auth: process.env.NOTION_KEY,
+  });
+
+  const pages = await notion.pages.retrieve({
+    page_id: process.env.PAGE_ID,
+  });
+  const blocks = await notion.blocks.children.list({
+    block_id: process.env.PAGE_ID,
+  });
+
+  return {
+    props: {
+      pages: pages,
+      blocks: blocks,
+    },
+  };
+}
+
+export default async function NotionTest() {
+  const data = await getData();
+
+  return (
+    <>
+      <pre>{JSON.stringify(data, undefined, 2)}</pre>
+    </>
+  );
+}
