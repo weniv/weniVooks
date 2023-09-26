4110dcae4a404a218d1857720ab887c69dfd75b7
nurimeansworld <nurimeansworld@gmail.com>
2023-09-26 14:29:31 +0900

    :white_check_mark: Test: 노션 API - database 단순 출력 내용 추가 #4"

--- src/app/notionTest/page.js
+++ src/app/notionTest/page.js
@@ -20,12 +20,34 @@ async function getData() {
   };
 }
 
+async function getDBData() {
+  const notion = new Client({
+    auth: process.env.NOTION_KEY,
+  });
+
+  const dbs = await notion.databases.retrieve({
+    database_id: process.env.DB_ID,
+  });
+
+  return {
+    dbs,
+  };
+}
+
 export default async function NotionTest() {
   const data = await getData();
+  const DBData = await getDBData();
 
   return (
     <>
-      <pre>{JSON.stringify(data, undefined, 2)}</pre>
+      <div>
+        <h2>위니북스 Page 결과값</h2>
+        <pre>{JSON.stringify(data, undefined, 2)}</pre>
+      </div>
+      <div>
+        <h2>위니북스 DB 결과값</h2>
+        <pre>{JSON.stringify(DBData, undefined, 2)}</pre>
+      </div>
     </>
   );
 }
