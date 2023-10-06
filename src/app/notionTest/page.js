68c386a44824468b245053bac846dcdb302675fe
nurimeansworld <nurimeansworld@gmail.com>
2023-10-06 15:31:13 +0900

    :white_check_mark: Add: notion to md 테스트 코드 추가 #4

--- src/app/notionTest/page.js
+++ src/app/notionTest/page.js
@@ -1,4 +1,5 @@
 import { Client } from '@notionhq/client';
+import { NotionToMarkdown } from 'notion-to-md';
 
 async function getData() {
   const notion = new Client({
@@ -36,12 +37,37 @@ async function getDBData() {
   };
 }
 
+async function getDataMd() {
+  const notion = new Client({ auth: process.env.NOTION_KEY });
+  const n2m = new NotionToMarkdown({
+    notionClient: notion,
+    // // 옵션들
+    // config: {
+    //   separateChildPage: true,
+    //   parseChildPages: false,
+    // },
+  });
+
+  // 테스트용으로 10/10 회의록 내용을 출력
+  const mdblocks = await n2m.pageToMarkdown(process.env.PAGE_ID_SAMPLE);
+  const mdstring = n2m.toMarkdownString(mdblocks).parent;
+
+  return {
+    mdstring,
+  };
+}
+
 export default async function NotionTest() {
   const data = await getData();
   const DBData = await getDBData();
+  const dataMd = await getDataMd();
 
   return (
     <>
+      <div>
+        <h2>위니북스 notion to md</h2>
+        <pre>{dataMd.mdstring}</pre>
+      </div>
       <div>
         <h2>위니북스 Page 결과값</h2>
         <pre>{JSON.stringify(data, undefined, 2)}</pre>
