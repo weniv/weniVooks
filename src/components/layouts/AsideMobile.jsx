f37ee4b9d8c94735af02c7b01d32e4fe9463a001
dreamfulbud <dreamfulbud@naver.com>
2023-11-07 10:20:54 +0900

    ✨ Feat: 페이지 목차 반응형

--- src/components/layouts/AsideMobile.jsx
+++ src/components/layouts/AsideMobile.jsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BtnIcon from '../common/button/BtnIcon';
 import SubBanner from './aside/SubBanner';
 import Toc from './aside/Toc';
@@ -13,34 +13,63 @@ export default function AsideMobile() {
   const [isMenuShow, setIsMenuShow] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuShow(!isMenuShow);
+    setIsMenuShow((prev) => !prev);
   };
+
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      const isAside = e.target.closest('aside');
+      if (isMenuShow && !isAside) {
+        toggleMenu(false);
+      }
+    };
+    const handleESC = (e) => {
+      if (isMenuShow && e.key === 'Escape') {
+        toggleMenu(false);
+      }
+    };
+
+    if (isMenuShow) {
+      setTimeout(() => {
+        window.addEventListener('click', handleOutsideClick);
+        window.addEventListener('keydown', handleESC);
+      }, 100);
+    }
+    return () => {
+      window.removeEventListener('click', handleOutsideClick);
+      window.removeEventListener('keydown', handleESC);
+    };
+  }, [isMenuShow]);
+
   return (
     <aside className={classNames(styles.aside, isMenuShow && styles.active)}>
-      {!isMenuShow ? (
-        <div className={styles.titleWrap}>
-          <h3>목차</h3>
-          <BtnIcon
-            onClick={toggleMenu}
-            children={<SVGDownArrow alt="열기" color="grayLv3" />}
-            bordernone="true"
-          />
-        </div>
-      ) : (
-        <div className={styles.inner}>
-          <h3>목차</h3>
+      <div className={styles.inner}>
+        {!isMenuShow ? (
+          <div className={styles.titleWrap}>
+            <h3>목차</h3>
+            <BtnIcon
+              type="button"
+              onClick={toggleMenu}
+              children={<SVGDownArrow alt="열기" color="grayLv3" />}
+              bordernone="true"
+            />
+          </div>
+        ) : (
+          <>
+            <h3>목차</h3>
 
-          <Toc />
+            <Toc />
 
-          <SubBanner />
-          <BtnIcon
-            onClick={toggleMenu}
-            children={<SVGUpArrow alt="닫기" color="grayLv3" />}
-            bordernone="true"
-            className={styles.btnClose}
-          />
-        </div>
-      )}
+            <SubBanner />
+            <BtnIcon
+              onClick={toggleMenu}
+              children={<SVGUpArrow alt="목차 닫기" color="grayLv3" />}
+              bordernone="true"
+              className={styles.btnClose}
+            />
+          </>
+        )}
+      </div>
     </aside>
   );
 }
