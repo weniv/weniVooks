6ccd4ab1636e2395be31a3a0e73e91da311d70a6
dreamfulbud <dreamfulbud@naver.com>
2023-11-27 14:40:01 +0900

    ✨ Feat: 코드 복사 기능

---
+++ src/components/common/button/BtnCopy.js
@@ -0,0 +1,32 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import styles from './BtnCopy.module.scss';
+import Btn from './Btn';
+import { handleClipBoard } from '@/utils/handleClipBoard';
+
+export default function BtnCopy() {
+  const [code, setCode] = useState(null);
+
+  useEffect(() => {
+    const codes = Array.from(
+      document.querySelectorAll('[data-rehype-pretty-code-fragment]'),
+    );
+
+    codes.forEach((code) => {
+      const button = document.createElement('button');
+      const span = document.createElement('span');
+      span.textContent = 'copy';
+      span.classList.add('a11y-hidden');
+      button.type = 'button';
+      button.classList.add(styles.copyBtn);
+      button.append(span);
+      button.addEventListener('click', (e) => {
+        const text = code.querySelector('pre').textContent;
+
+        handleClipBoard(text);
+      });
+      code.append(button);
+    });
+  }, []);
+}
