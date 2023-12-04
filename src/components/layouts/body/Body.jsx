fd6cd780bea2e5f1a642900346add231115ea3a5
dreamfulbud <dreamfulbud@naver.com>
2023-12-04 10:06:31 +0900

    ♻️ Chore: 코드 정리

--- src/components/layouts/body/Body.jsx
+++ src/components/layouts/body/Body.jsx
@@ -3,9 +3,36 @@ import { SettingContext } from '@/context/SettingContext';
 import classNames from 'classnames';
 import { useContext, useEffect, useState } from 'react';
 
+const BeforeFn = () => {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme !== 'auto' && savedTheme !== null) {
+    document.body.classList.add(savedTheme);
+  }
+
+  const savedFontStyle = localStorage.getItem('fontStyle');
+  if (savedFontStyle !== null) {
+    document.body.classList.add(savedFontStyle);
+  }
+
+  const savedFontSize = localStorage.getItem('fontSize');
+
+  if (savedFontSize !== null) {
+    document.body.classList.add('size' + savedFontSize);
+  }
+
+  const savedOpen = localStorage.getItem('menu');
+
+  if (savedOpen === 'close') {
+    document.body.classList.add('side-close');
+  } else if (savedOpen === 'open') {
+    document.body.classList.add('side-open');
+  }
+};
+
 export default function Body({ children }) {
   const { theme, fontStyle, fontSize, menu } = useContext(SettingContext);
   const [isWindows, setIsWindows] = useState(false);
+
   useEffect(() => {
     if (navigator.userAgent.includes('Windows')) {
       setIsWindows(true);
@@ -18,13 +45,18 @@ export default function Body({ children }) {
         theme !== 'auto' && theme,
         fontStyle,
         fontSize !== null && `size${fontSize}`,
-
         menu === null ? `side-open` : `side-${menu}`,
         isWindows && 'windows',
       )}
       suppressHydrationWarning={true}
     >
       {children}
+
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(${String(BeforeFn)})()`,
+        }}
+      ></script>
     </body>
   );
 }
