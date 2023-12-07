dee4cee1d2d94578354dd8e52b3d02fcdc0171ca
min-bok <leehy0782@gmail.com>
2023-12-07 17:58:36 +0900

    💄UI: 검색 결과 페이지 이동 버튼 추가

--- src/app/search/layout.jsx
+++ src/app/search/layout.jsx
@@ -1,3 +1,4 @@
+'use client';
 import Header from '@/components/layouts/Header';
 import BtnTop from '@/components/feature/BtnTop';
 
@@ -5,8 +6,16 @@ import styles from '../search/layout.module.scss';
 import searchStyles from '../search/search.module.scss';
 import classNames from 'classnames';
 import Footer from '@/components/layouts/Footer';
+import Btn from '@/components/common/button/Btn';
+import SVGPrevArrow from '@/components/svg/SVGPrevArrow';
+import SVGNextArrow from '@/components/svg/SVGNextArrow';
+import useWindowSize from '@/context/useWindowSize';
 
 export default function Layout({ children }) {
+  const windowSize = useWindowSize();
+  const prev = true;
+  const next = true;
+
   return (
     <>
       <div className={classNames('layout-grow', styles.wrapper)}>
@@ -17,6 +26,16 @@ export default function Layout({ children }) {
         <Footer intro={true} />
       </div>
 
+      <div className={styles.btnWrap}>
+        <Btn className={styles.btnPrev} disabled={!prev}>
+          <SVGPrevArrow color="grayLv3" />
+          {windowSize > 1024 && <span>{'이전'}</span>}
+        </Btn>
+        <Btn className={styles.btnNext} disabled={!next}>
+          {windowSize > 1024 && <span>{'다음'}</span>}
+          <SVGNextArrow color="grayLv3" />
+        </Btn>
+      </div>
       <BtnTop />
     </>
   );
