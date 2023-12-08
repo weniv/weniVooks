487cbc92494b39f8c889a8158acb35a542ec1905
dreamfulbud <dreamfulbud@naver.com>
2023-12-08 14:49:09 +0900

    🐛 Fix: 메인 Top 버튼 누락 수정 및 CSS z-index 수정

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import Footer from '@/components/layouts/footer/Footer';
 import Banner from '@/components/banner/Banner';
 import BookList from '@/components/book/BookList';
 import Header from '@/components/layouts/header/Header';
+import BtnTop from '@/components/common/button/BtnTop';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <BookList />
       </div>
       <Footer intro={true} />
+      <BtnTop />
     </>
   );
 }
