d2e198adaf14a5f485bad504264fecc6d1d2b9e8
dreamfulbud <dreamfulbud@naver.com>
2023-12-12 11:34:11 +0900

    🚧 Ing: 전체메뉴 수정 작업중

--- src/components/layouts/menu/Side.jsx
+++ src/components/layouts/menu/Side.jsx
@@ -15,97 +15,42 @@ import { usePathname } from 'next/navigation';
 
 export default function Side(props) {
   const path = usePathname();
+
   const { windowWidth } = useWindowSize();
-  const { menu, setMenu } = useContext(SettingContext);
-  const [slide, setSlide] = useState(null);
-  const [isDOM, setIsDOM] = useState(menu === 'close' ? false : true);
 
-  const slideIn = () => {
-    setIsDOM(true);
-    setMenu('open');
-    setSlide('slideIn');
+  const [isMenuOpen, setIsMesuOpen] = useState(false);
+  const [isInDOM, setIsInDOM] = useState(false);
 
-    localStorage.removeItem('menu');
-  };
+  const toggleMenu = () => {
+    if (isMenuOpen) {
+      // 열림 -> 닫힘
+      setIsMesuOpen(false);
 
-  const slideOut = () => {
-    setMenu('close');
-    setSlide('slideOut');
-    setTimeout(() => {
-      setIsDOM(false);
-    }, 300);
-    localStorage.setItem('menu', 'close');
-  };
-
-  useEffect(() => {
-    if (windowWidth !== null && windowWidth <= 1024) {
-      setMenu('close');
-      setIsDOM(false);
-      localStorage.setItem('menu', 'close');
-    }
-  }, [windowWidth, path]);
-
-  useEffect(() => {
-    if (menu === 'open' && windowWidth <= 1024) {
-      document.body.style.cssText = `
-      overflow: hidden;
-      position: relative;
-      height: 100%;`;
+      windowWidth > 1024 && localStorage.setItem('menu', 'close');
     } else {
-      document.body.removeAttribute('style');
-    }
-    return () => {
-      document.body.removeAttribute('style');
-    };
-  }, [menu, windowWidth]);
+      // 닫힘 -> 열림
+      setIsMesuOpen(true);
 
+      windowWidth > 1024 && localStorage.removeItem('menu');
+    }
+  };
   return (
     <>
-      {isDOM && (
-        <>
-          <div
-            className={classNames(
-              'layout-side',
-              styles.side,
-              slide === null
-                ? ''
-                : slide === 'slideIn'
-                ? styles.menuShow
-                : styles.menuHide,
-            )}
-          >
-            <Nav {...props} />
-            <BtnIcon
-              className={styles.btnClose}
-              children={<SVGListClose color="grayLv3" />}
-              onClick={slideOut}
-              bordernone="true"
-            />
-            {windowWidth >= 1024 && <Footer />}
-          </div>
-          {menu === 'open' && windowWidth < 1024 && (
-            <div
-              className={classNames(
-                'dim',
-                slide === 'slideIn' ? styles.dimShow : styles.dimHide,
-              )}
-              onClick={slideOut}
-            ></div>
-          )}
-        </>
-      )}
-
-      {!(menu === 'open' && windowWidth >= 1024) && (
-        <BtnIcon
-          className={classNames(
-            styles.openBtn,
-            menu == 'close' ? styles['openBtn-show'] : styles['openBtn-hide'],
-          )}
-          children={<SVGList color="grayLv3" />}
-          onClick={slideIn}
-          bordernone="true"
-          disabled={menu === 'open' ? true : false}
-        />
+      {isMenuOpen ? (
+        <div className={classNames(styles.side)}>
+          <Nav {...props} />
+          <BtnIcon
+            className={styles.btnClose}
+            children={<SVGListClose color="grayLv3" />}
+            bordernone="true"
+            onClick={toggleMenu}
+          />
+        </div>
+      ) : (
+        <button type="button" className={styles.btnOpen} onClick={toggleMenu}>
+          <SVGList color="grayLv3" />
+          <span className="a11y-hidden">메뉴 열기</span>
+        </button>
       )}
     </>
   );
