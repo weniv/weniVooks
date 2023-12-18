9d48e0bd4e8deee25311d4dcc649ef7666d23bed
min-bok <leehy0782@gmail.com>
2023-12-18 15:59:05 +0900

    👷Fix: next.config.js 파일 수정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Set this to 'export' to enable static site generation
+  // when running the `next export` command.
+  output: {
+    export: {},
+  },
+  // Check for other experimental features and update as needed
+  // experimental: {
+  //   images: true,
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
