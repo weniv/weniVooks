a867f7259c127767e6656942fe966fb1dc59e0ab
dreamfulbud <dreamfulbud@naver.com>
2024-01-17 09:19:43 +0900

    ✨ Feat: 서브배너 추가 및 랜덤 노출

--- src/components/layouts/aside/SubBanner.jsx
+++ src/components/layouts/aside/SubBanner.jsx
@@ -4,19 +4,24 @@ import styles from './SubBanner.module.scss';
 import banner from '@/data/subBanner.json';
 
 export default function SubBanner() {
-  return (
-    <div className={styles.banner}>
-      <ol>
-        {banner.map((banner, index) => (
-          <li key={index}>
-            <a href="#">
-              <h3>{banner.title}</h3>
-              <p>{banner.subText}</p>
-              <Image src={banner.src} alt="" width={112} height={112} />
-            </a>
-          </li>
-        ))}
-      </ol>
-    </div>
-  );
+  const randomNum = Math.floor(Math.random() * banner.length);
+
+  if (banner.length > 0) {
+    return (
+      <div className={styles.banner}>
+        <a
+          href="#"
+          style={{
+            backgroundColor:
+              banner[randomNum].bgColor && banner[randomNum].bgColor,
+            color: banner[randomNum].textColor && banner[randomNum].textColor,
+          }}
+        >
+          <h3>{banner[randomNum].title}</h3>
+          <p>{banner[randomNum].subText}</p>
+          <Image src={banner[randomNum].src} alt="" width={112} height={112} />
+        </a>
+      </div>
+    );
+  }
 }
