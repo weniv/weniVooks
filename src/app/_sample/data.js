72b299113fc33796ca26bd284600d55430a5b60d
dreamfulbud <dreamfulbud@naver.com>
2024-01-18 10:48:53 +0900

    📝 Edit: 메타태그 수정(이미지, twitter 추가)

--- src/app/_sample/data.js
+++ src/app/_sample/data.js
@@ -2,7 +2,10 @@ export const DEFAULT_PATH = ''; // '/'로 시작해주세요 ex. '/python'
 export const TITLE = '';
 export const DESC = '';
 
-export const OGIMG = [`https://books.weniv.co.kr/images${DEFAULT_PATH}/og.png`];
+export const OGIMG = [
+  `https://books.weniv.co.kr/images${DEFAULT_PATH}/og.png`,
+  `https://books.weniv.co.kr/images/opengraph-image.png`,
+];
 export const META_DATA = {
   title: `${TITLE} | 위니북스`,
   description: DESC,
