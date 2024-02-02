4898d5412dbbf111e2c15c5103141692ec37cf4e
min-bok <leehy0782@gmail.com>
2024-02-02 15:42:11 +0900

    ♻️Refactor: 각각의 파이썬 코드블럭이 독립적으로 실행되도록 수정 - 현재는 shift enter의 경우에만 올바른 결과값이 실행됨 - 해당 코드 블럭의 코드만 복사되도록 코드 복사 코드 수정

--- src/app/codeblock/page.jsx
+++ src/app/codeblock/page.jsx
@@ -1,6 +1,8 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import { useState } from 'react';
+import { getInitPythonCode } from '@/components/codeblock/event';
 
 const PythonCodeblock = dynamic(
   () => import('@/components/codeblock/PythonCodeblock'),
@@ -17,17 +19,35 @@ const JavascriptCodeblock = dynamic(
 );
 
 export default function page() {
+  const initCode = `def solution():\n  return "Hello World"\nsolution()`;
+  const [python, setPython] = useState([
+    { id: 1, code: initCode, result: '결과값' },
+    {
+      id: 2,
+      code: `def solution():\n  return "Hello 2222"\nsolution()`,
+      result: '111',
+    },
+  ]);
+
   return (
     <>
-      <div>
+      <div style={{ overflowX: 'hidden' }}>
         <h2>Python</h2>
-        <PythonCodeblock />
+        {python.map((el) => (
+          <PythonCodeblock
+            key={el.id}
+            id={el.id}
+            code={el.code}
+            result={el.result}
+            setPython={setPython}
+          />
+        ))}
       </div>
 
-      <div>
+      {/* <div>
         <h2>JavaScript</h2>
         <JavascriptCodeblock />
-      </div>
+      </div> */}
     </>
   );
 }
