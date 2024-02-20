98b0847e3dbf8e4cf1c6edd44c8482bf102144a0
min-bok <leehy0782@gmail.com>
2024-02-20 12:37:50 +0900

    ✅Test: 자동배포 테스트 4

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Header type="intro" />
       <div className="layout-grow">
         <Banner />
-        <h1>test444</h1>
+        <h1>test555</h1>
         <BookList />
       </div>
       <Footer intro={true} />
