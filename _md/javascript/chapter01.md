ebd164da4046a4a229b6602baec99f92e13a3dfb
baekg110 <baekg.dev@gmail.com>
2024-04-29 13:21:23 +0900

    :memo: Docs: 자바스크립트 에센셜 chapter01

---
+++ _md/javascript/chapter01.md
@@ -0,0 +1,13 @@
+---
+title: 1. JavaScript 기초
+date: 2024-04-29
+---
+
+:::div{.box .list}
+**목차**
+
+1. [JavaScript란?](/javascript/chapter01/01-1)
+2. [JavaScript 사용하기](/javascript/chapter01/01-2)
+3. [브라우저에서 실행하기](/javascript/chapter01/01-3)
+
+:::
---
+++ src/app/javascript/[...slug]/page.jsx
@@ -0,0 +1,71 @@
+import { getPostDetail } from '@/utils/getPosts';
+import { DEFAULT_PATH, TITLE, DESC } from '../data';
+import { JSDOM } from 'jsdom';
+import Script from 'next/script';
+import '../../../../public/codeblocks/common.css';
+import '../../../../public/codeblocks/codemirror.css';
+
+function replaceCodeWithJsRepl(htmlString) {
+  const dom = new JSDOM(htmlString);
+
+  const codeElements = dom.window.document.querySelectorAll(
+    'pre.weniv-light[data-language="javascript-exec"]',
+  );
+
+  let deleteElements = dom.window.document.querySelectorAll(
+    'pre.weniv-dark[data-language="javascript-exec"]',
+  );
+
+  deleteElements.forEach((el) => {
+    el.remove();
+  });
+
+  codeElements.forEach((el) => {
+    const content = el.textContent;
+    const jsReplElement = dom.window.document.createElement('js-repl');
+    jsReplElement.textContent = content;
+    el.replaceWith(jsReplElement);
+  });
+
+  return dom.serialize();
+}
+
+export async function generateMetadata({ params }, parent) {
+  const { title } = await getPostDetail(DEFAULT_PATH, params.slug);
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    metadataBase: new URL(`https://books.weniv.co.kr${DEFAULT_PATH}`),
+    title: `${title ? title + ' | ' : ''} ${TITLE}`,
+    openGraph: {
+      type: 'website',
+      title: `${title ? title + ' | ' : ''} ${TITLE}`,
+      description: DESC,
+      // url: `${url}`,
+      siteName: TITLE,
+      images: [`/images${DEFAULT_PATH}/og.png`, ...previousImages],
+    },
+  };
+}
+
+export default async function Page({ params }) {
+  const { title, htmlContent } = await getPostDetail(DEFAULT_PATH, params.slug);
+  const replacedHtmlContent = replaceCodeWithJsRepl(htmlContent);
+
+  return (
+    <>
+      {htmlContent && (
+        <>
+          <h3 className="title">{title}</h3>
+          <div dangerouslySetInnerHTML={{ __html: replacedHtmlContent }} />
+        </>
+      )}
+      <link rel="stylesheet" href="/codeblocks/codemirror.css" />
+      <Script src="/codeblocks/codemirror.js" />
+      <Script defer src="/codeblocks/javascript/js-repl.js" />
+      <Script defer src="/codeblocks/codemirror.js" />
+      <Script defer src="/codeblocks/javascript/javascript.js" />
+      <Script defer src="/codeblocks/codemirror/active-line.js" />
+    </>
+  );
+}
