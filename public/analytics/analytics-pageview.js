933d7edccaf1943c2a04fbf078aff166c1687059
dreamfulbud <dreamfulbud@naver.com>
2024-05-03 11:16:18 +0900

    📝 Edit: 코드 수정

--- public/analytics/analytics-pageview.js
+++ public/analytics/analytics-pageview.js
@@ -1,8 +1,22 @@
 // 페이지뷰 데이터 전송
-const session_id = sessionStorage.getItem('session_id');
-const referrer = document.referrer;
+let session_id = sessionStorage.getItem('session_id');
 
-if (!session_id && referrer === '') {
+if (session_id) {
+  fetch('https://www.analytics.weniv.co.kr/collect/pageview', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ url: window.location.href, session_id: session_id }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .catch((error) => console.error('Error:', error));
+} else {
   fetch('https://www.analytics.weniv.co.kr/collect/pageview', {
     method: 'POST',
     headers: {
@@ -17,7 +31,6 @@ if (!session_id && referrer === '') {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
       sessionStorage.setItem('session_id', data.session_id);
     })
     .catch((error) => console.error('Error:', error));
