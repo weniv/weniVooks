633468f845c0d2cd480a3fc4b6b5984ede494bb9
dreamfulbud <dreamfulbud@naver.com>
2024-05-07 15:58:55 +0900

    🐛 Fix: 422 에러 - 링크 이동 버그 수정

--- src/utils/handleAnalyticsClick.js
+++ src/utils/handleAnalyticsClick.js
@@ -1,6 +1,6 @@
 //  클릭 이벤트 리스너 등록
 
-export default function handleAnalyticsClick(event, text) {
+export default async function handleAnalyticsClick(event, text) {
   event.preventDefault(); // 기본 동작 막기
 
   const ANCHOR = event.currentTarget;
@@ -11,22 +11,25 @@ export default function handleAnalyticsClick(event, text) {
   const target_url = ANCHOR.href;
   const target_tar = ANCHOR.target || '_self';
 
-  fetch('https://www.analytics.weniv.co.kr/collect/anchor-click', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Session-Id': session_id,
-    },
-    body: JSON.stringify({ source_url, target_url, type: text }),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(() => {
-      window.open(target_url, target_tar);
-    })
-    .catch((error) => console.error('Error:', error));
+  try {
+    const response = await fetch(
+      'https://www.analytics.weniv.co.kr/collect/anchor-click',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Session-Id': session_id,
+        },
+        body: JSON.stringify({ source_url, target_url, type: text }),
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  } finally {
+    window.open(target_url, target_tar);
+  }
 }
