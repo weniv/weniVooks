1076494b74721f8459a822e8528f644bd5de6e3d
dreamfulbud <dreamfulbud@naver.com>
2024-06-20 11:55:09 +0900

    📝 Edit: 파일경로 추가

--- src/app/polars/layout.jsx
+++ src/app/polars/layout.jsx
@@ -1,26 +1,23 @@
 import '@/styles/sub.scss';
 
+import { DEFAULT_PATH, META_DATA, TITLE } from './data';
+import menuDdata from '@/data/menu/polars.json';
+
 import Header from '@/components/layouts/header/Header';
 import Side from '@/components/layouts/menu/Side';
 import Page from '@/components/layouts/pagecontrol/PageControl';
 import BtnTop from '@/components/common/button/BtnTop';
-
-import { DEFAULT_PATH, META_DATA, TITLE } from './data';
-import { getData } from '@/utils/getData';
 import Breadcrumb from '@/components/layouts/breadcrumb/Breadcrumb';
 
 export const metadata = META_DATA;
 
 export default async function Layout({ children }) {
-  const menuDdata = await getData(DEFAULT_PATH);
-
   return (
     <>
       <Header />
 
       <div className="sub">
         <h2 className="a11y-hidden">{TITLE}</h2>
-
         <Side data={menuDdata} />
         <div className="sub__content">
           <Breadcrumb data={menuDdata} DEFAULT_PATH={DEFAULT_PATH} />
