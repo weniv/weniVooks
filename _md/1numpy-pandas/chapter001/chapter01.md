6a13e5741db9c87d4802cabadba107db27dd1522
Kim Myeonghoon <jnuhoon@gmail.com>
2024-07-04 17:58:58 +0900

    넘파이 초기 및 환경 설정, 페이지 표시 준비 단계

---
+++ _md/1numpy-pandas/chapter001/chapter01.md
@@ -0,0 +1,11 @@
+---
+title: 개발의 첫 걸음
+date: 2024-07-04
+---
+
+:::div{.box .list}
+**목차**
+
+1. [넘파이란?](/basecamp-html-css/chapter01/01-1)
+
+:::
---
+++ src/app/1numpy-pandas/data.js
@@ -0,0 +1,34 @@
+export const DEFAULT_PATH = "";
+export const TITLE = "";
+export const DESC = "";
+export const OGIMG = [`/images${DEFAULT_PATH}/og.png`];
+export const META_DATA = {
+  title: `${TITLE} | 위니북스`,
+  description: DESC,
+  openGraph: {
+    type: "website",
+    title: `${TITLE} | 위니북스`,
+    description: DESC,
+    url: `https://books.weniv.co.kr${DEFAULT_PATH}`,
+    siteName: TITLE,
+    images: OGIMG,
+  },
+};
+
+// // 예시
+// export const DEFAULT_PATH = "/wenivworld";
+// export const TITLE = "위니브 월드(학생용)";
+// export const DESC = "위니브월드로 떠나는 파이썬 코딩 여행";
+// export const OGIMG = [`/images${DEFAULT_PATH}/og.png`];
+// export const META_DATA = {
+//   title: `${TITLE} | 위니북스`,
+//   description: DESC,
+//   openGraph: {
+//     type: "website",
+//     title: `${TITLE} | 위니북스`,
+//     description: DESC,
+//     url: `https://books.weniv.co.kr${DEFAULT_PATH}`,
+//     siteName: TITLE,
+//     images: OGIMG,
+//   },
+// };
