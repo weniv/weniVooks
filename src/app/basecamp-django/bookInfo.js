91c5079d8d1e0d45224689712aeec18cca64d2d4
dreamfulbud <dreamfulbud@naver.com>
2024-07-05 09:36:16 +0900

    ♻️ Refactor: 코드 변경

---
+++ src/app/basecamp-django/bookInfo.js
@@ -0,0 +1,10 @@
+export const DEFAULT_PATH = 'basecamp-django';
+export const TITLE = 'Django 베이스캠프';
+export const DESCRIPTION = '위니브의 다양한 교안을 웹에서 확인해보세요!';
+
+// 'HTML/CSS', 'JavaScript', 'Python'
+export const EDITOR = null;
+
+// 다를 경우 수정하세요! 경로에 알맞은 형식으로 넣었다면 수정할 필요 없습니다.
+export const COVER = `/images/${DEFAULT_PATH}/cover.png`;
+export const OG_IMG = `/images/opengraph-image.png`;
