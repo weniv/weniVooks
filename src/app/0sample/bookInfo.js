de3548c70019002aa81cc0a4c4a1bbaf45b29bd6
dreamfulbud <dreamfulbud@naver.com>
2024-07-05 15:28:50 +0900

    ♻️ Refactor: 코드 변경

--- src/app/0sample/bookInfo.js
+++ src/app/0sample/bookInfo.js
@@ -3,8 +3,8 @@ export const TITLE = 'HTML/CSS 베이스캠프 테스트';
 export const DESCRIPTION = '위니브의 다양한 교안을 웹에서 확인해보세요!';
 
 // 필요시 배열로 삽입 ['HTML/CSS', 'JavaScript', 'Python']
-export const EDITOR = null;
+export const EDITOR = ['HTML/CSS'];
 
 // 다를 경우 수정하세요! 경로에 알맞은 형식으로 넣었다면 수정할 필요 없습니다.
-export const COVER = `/images/${DEFAULT_PATH}/cover-react.png`;
+export const COVER = `/images/${DEFAULT_PATH}/cover.png`;
 export const OG_IMG = `/images/opengraph-image.png`;
---
+++ src/app/basecamp-html-css/bookInfo.js
@@ -0,0 +1,11 @@
+export const DEFAULT_PATH = 'basecamp-html-css';
+export const TITLE = 'HTML/CSS 베이스캠프';
+export const DESCRIPTION =
+  '웹 페이지의 구조를 정의하는 HTML과 웹 페이지의 스타일을 정의하는 CSS로 다양한 콘텐츠와 디자인을 구현할 수 있습니다.';
+
+// 필요시 배열로 삽입 ['HTML/CSS', 'JavaScript', 'Python']
+export const EDITOR = ['HTML/CSS'];
+
+// 다를 경우 수정하세요! 경로에 알맞은 형식으로 넣었다면 수정할 필요 없습니다.
+export const COVER = `/images/${DEFAULT_PATH}/cover-html-css.png`;
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover-html-css.png`;
