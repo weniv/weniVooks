09f9863b12f1425cf4bfb1566923f13d2c429c72
dreamfulbud <dreamfulbud@naver.com>
2024-07-08 16:10:13 +0900

    📝 Edit: 기본값 수정

--- src/app/0sample/bookInfo.js
+++ src/app/0sample/bookInfo.js
@@ -2,9 +2,8 @@ export const DEFAULT_PATH = '0sample';
 export const TITLE = 'HTML/CSS 베이스캠프 테스트';
 export const DESCRIPTION = '위니브의 다양한 교안을 웹에서 확인해보세요!';
 
-// 필요시 배열로 삽입 ['HTML/CSS', 'JavaScript', 'Python']
-export const EDITOR = ['HTML/CSS'];
+export const COVER = `/images/${DEFAULT_PATH}/cover.png`; // 책 표지
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover.png`; // 링크 공유 시 나올 이미지
 
-// 다를 경우 수정하세요! 경로에 알맞은 형식으로 넣었다면 수정할 필요 없습니다.
-export const COVER = `/images/${DEFAULT_PATH}/cover.png`;
-export const OG_IMG = `/images/opengraph-image.png`;
+// 필요시 배열로 삽입 ['HTML/CSS', 'JavaScript', 'Python']
+export const EDITOR = ['HTML/CSS', 'JavaScript'];
