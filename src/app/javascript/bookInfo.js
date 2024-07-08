70785b27c1fd09f2f5d96eaa822b1ff08b410ab8
dreamfulbud <dreamfulbud@naver.com>
2024-07-08 16:11:05 +0900

    ♻️ Refactor: 코드 변경

---
+++ src/app/javascript/bookInfo.js
@@ -0,0 +1,9 @@
+export const DEFAULT_PATH = 'javascript';
+export const TITLE = 'JavaScript 에센셜';
+export const DESCRIPTION =
+  'JavaScript의 기초부터 심화까지 필수적인 내용을 담았습니다!';
+
+// 필요시 배열로 삽입 ['HTML/CSS', 'JavaScript', 'Python']
+export const EDITOR = ['JavaScript'];
+export const COVER = `/images/${DEFAULT_PATH}/cover-${DEFAULT_PATH}.png`;
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover-${DEFAULT_PATH}.png`;
