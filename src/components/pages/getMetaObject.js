4762c971ca4c5ec7bc872339e9c95e8467a2e653
dreamfulbud <dreamfulbud@naver.com>
2024-07-15 10:00:07 +0900

    📝 Edit: 메타 태그 수정

--- src/components/pages/getMetaObject.js
+++ src/components/pages/getMetaObject.js
@@ -5,8 +5,15 @@ const getMetaObject = (TITLE, DESCRIPTION, DEFAULT_PATH, IMG) => {
     openGraph: {
       type: 'website',
       title: `${TITLE} | 위니북스`,
+      description: DESCRIPTION,
       url: `https://books.weniv.co.kr/${DEFAULT_PATH}`,
       siteName: '위니북스',
+      images: [IMG],
+    },
+    twitter: {
+      card: 'summary',
+      title: `${TITLE} | 위니북스`,
+      description: DESCRIPTION,
       images: [IMG],
     },
   };
