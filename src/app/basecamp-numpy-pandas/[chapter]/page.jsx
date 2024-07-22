5f11c27d71906b721e14b879c58a4ad91d818872
baekg110 <baekg.dev@gmail.com>
2024-07-22 18:08:38 +0900

    :bug: numpy, pandas로 인한 배포 오류 수정

--- src/app/basecamp-numpy-pandas/[chapter]/page.jsx
+++ src/app/basecamp-numpy-pandas/[chapter]/page.jsx
@@ -1,14 +1,10 @@
 import { DEFAULT_PATH, TITLE } from '../bookInfo';
 import { getChapters } from '@/sub/getChapters';
 import ChapterPage from '@/components/pages/ChapterPage';
-import 'numpy';
-import 'pandas';
-
 
 const json = {
   package: ['numpy', 'pandas'],
 };
-
 
 // 챕터 페이지
 export default function page({ params }) {
