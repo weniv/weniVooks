a959fd2c48e362e033ac0ce240a778bb0ba13840
paullabkorea <paullabkorea@gmail.com>
2024-07-23 23:40:45 +0900

    파이썬 베이스캠프 기본 세팅

---
+++ src/app/basecamp-python/bookInfo.js
@@ -0,0 +1,9 @@
+export const DEFAULT_PATH = 'basecamp-python';
+export const TITLE = '베이스캠프 파이썬';
+export const DESCRIPTION =
+  '파이썬을 쉽고 재미있게 배우는 베이스캠프 파이썬!';
+
+export const COVER = `/images/${DEFAULT_PATH}/cover.png`; // 책 표지
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover.png`; // 링크 공유 시 나올 이미지
+
+export const EDITOR = ['Python'];
