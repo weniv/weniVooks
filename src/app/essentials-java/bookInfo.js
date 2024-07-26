7dfea09ba7d9eba352fd564c7086d3fe6614d429
unknown <elwl5515@gmail.com>
2024-07-26 17:22:14 +0900

    📝 ADD: java essentials 제목 수정 & Chapter 6~18 목차, 세부 목차 추가

---
+++ src/app/essentials-java/bookInfo.js
@@ -0,0 +1,9 @@
+export const DEFAULT_PATH = 'essentials-java';
+export const TITLE = 'Java 에센셜';
+export const DESCRIPTION = 'Java 프로그래밍 언어의 기초 개념을 다루는 학습 가이드입니다.';
+
+export const COVER = `/images/${DEFAULT_PATH}/cover.png`; // 책 표지
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover.png`; // 링크 공유 시 나올 이미지
+
+// 'HTML/CSS', 'Python', 'JavaScript' 중 선택하여 삽입해주세요
+export const EDITOR = ['HTML/CSS'];
