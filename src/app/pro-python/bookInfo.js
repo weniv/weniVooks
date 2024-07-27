28831d36db50a9bf9f87e38e954a244a3f7894f8
paullabkorea <paullabkorea@gmail.com>
2024-07-27 17:21:38 +0900

    폴더명 및 모든 목차 수정과 견고한 파이썬 챕터 3까지 수정

---
+++ src/app/pro-python/bookInfo.js
@@ -0,0 +1,9 @@
+export const DEFAULT_PATH = 'pro-python';
+export const TITLE = '견고한 파이썬';
+export const DESCRIPTION =
+  '웹/앱, 인공지능, 게임 개발, 데이터 분석, 업무 자동화 등 다양한 분야에서 활용되는 프로그래밍 언어 파이썬을 견고하게 배워보자!';
+
+export const COVER = `/images/${DEFAULT_PATH}/cover-${DEFAULT_PATH}.png`; // 책 표지
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover-${DEFAULT_PATH}.png`; // 링크 공유 시 나올 이미지
+
+export const EDITOR = ['Python'];
