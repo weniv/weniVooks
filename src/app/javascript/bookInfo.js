1eb6d6fba6a5278dbfbd7f07d3777fec8159936b
baekg110 <baekg.dev@gmail.com>
2024-07-29 09:29:53 +0900

    :truck: Rename: 자바스크립트  에센셜 이미지 폴더명 수정

--- src/app/javascript/bookInfo.js
+++ src/app/javascript/bookInfo.js
@@ -1,4 +1,4 @@
-export const DEFAULT_PATH = 'javascript';
+export const DEFAULT_PATH = 'essentials-javascript';
 export const TITLE = 'JavaScript 에센셜';
 export const DESCRIPTION =
   'JavaScript의 기초부터 심화까지 필수적인 내용을 담았습니다!';
