503f75c2a6ad8e040c658bd50d4643e8425c31ba
rosamondkim <itzmems@naver.com>
2024-07-29 13:56:26 +0900

    :truck: Rename: html/css 에센셜 og및 커버이미지 경로변경

--- src/app/essentials-html-css/bookInfo.js
+++ src/app/essentials-html-css/bookInfo.js
@@ -2,8 +2,10 @@ export const DEFAULT_PATH = 'essentials-html-css';
 export const TITLE = 'HTML/CSS 에센셜';
 export const DESCRIPTION = '위니브의 다양한 교안을 웹에서 확인해보세요!';
 
-export const COVER = '/images/html-css/cover-essential-html-css.png';
-export const OG_IMG = '/images/html-css/cover-essential-html-css.png';
+export const COVER =
+  '/images/essentials-html-css/cover-essentials-html-css.png';
+export const OG_IMG =
+  '/images/essentials-html-css/cover-essentials-html-css.png';
 
 // 'HTML/CSS', 'Python', 'JavaScript' 중 선택하여 삽입해주세요
 export const EDITOR = ['HTML/CSS'];
