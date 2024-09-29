4d4d425ca3d35deeede18314a2d135bbec6ea20c
paullabkorea <paullabkorea@gmail.com>
2024-09-29 11:03:16 +0900

    데이터분석 베이스캠프 교안 push

---
+++ src/app/basecamp-data-analysis/bookInfo.js
@@ -0,0 +1,8 @@
+export const DEFAULT_PATH = 'basecamp-data-analysis';
+export const TITLE = 'basecamp-data-analysis';
+export const DESCRIPTION = '위니브의 다양한 교안을 웹에서 확인해보세요!';
+
+export const COVER = `/images/basecamp-data-analysis/basecamp-data-analysis.png`; // 책 표지
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover.png`; // 링크 공유 시 나올 이미지
+
+export const EDITOR = ['Python'];
