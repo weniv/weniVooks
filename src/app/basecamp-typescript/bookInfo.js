dc4c809521b7d1e6793a2f451f4bd47bd9224bf5
WonBeomChoi <cwb2310@gmail.com>
2024-10-02 15:38:06 +0900

    타입스크립트 교안 추가
    추가 내용 작성예정

---
+++ src/app/basecamp-typescript/bookInfo.js
@@ -0,0 +1,9 @@
+export const DEFAULT_PATH = 'basecamp-typescript';
+export const TITLE = '타입스크립트 베이스캠프';
+export const DESCRIPTION = '타입스크립트 기초';
+
+export const COVER = `/images/${DEFAULT_PATH}/cover.png`; // 책 표지
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover.png`; // 링크 공유 시 나올 이미지
+
+// 'HTML/CSS', 'Python', 'JavaScript' 중 선택하여 삽입해주세요
+export const EDITOR = [];
