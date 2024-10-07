b8eec1200a16e3c844ef846243ff8bec554a0aa9
paullabkorea <paullabkorea@gmail.com>
2024-10-07 21:39:57 +0900

    fastapi 베이스캠프 내용 추가

--- src/app/basecamp-fastapi/bookInfo.js
+++ src/app/basecamp-fastapi/bookInfo.js
@@ -3,7 +3,7 @@ export const TITLE = 'FastAPI 베이스캠프';
 export const DESCRIPTION = '위니브의 다양한 교안을 웹에서 확인해보세요!';
 
 export const COVER = `/images/${DEFAULT_PATH}/cover-fastapi.png`;
-export const OG_IMG = `/images/${DEFAULT_PATH}/cover.png`; // 링크 공유 시 나올 이미지
+export const OG_IMG = `/images/${DEFAULT_PATH}/og.png`; // 링크 공유 시 나올 이미지
 
 // 'HTML/CSS', 'Python', 'JavaScript' 중 선택하여 삽입해주세요
 export const EDITOR = [];
