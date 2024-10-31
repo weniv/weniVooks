50fcb340ba23a8d3b64d58a3a8a91cb5bafa11c8
Iam-Ujin <dklvmf0106@naver.com>
2024-10-31 11:28:26 +0900

    ✨ Edit: 복사 버튼 클릭 후 alet 제거, tooltip UI 추가

--- src/components/sub/common/CodeCopyBtn.jsx
+++ src/components/sub/common/CodeCopyBtn.jsx
@@ -1,20 +1,34 @@
 import styles from './CodeBtn.module.scss';
-
 import Icon from '@/components/icon/Icon';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 export default function CodeCopyBtn({ code }) {
+  const [showNotification, setShowNotification] = useState(false);
+
   const copyCode = useCallback(() => {
     navigator.clipboard
       .writeText(code)
-      .then(() => alert('코드가 클립보드에 복사되었습니다.'))
+      .then(() => {
+        setShowNotification(true);
+
+        setTimeout(() => {
+          setShowNotification(false);
+        }, 2000);
+      })
       .catch((err) => console.error('복사 실패:', err));
   }, [code]);
 
   return (
-    <button type="button" onClick={() => copyCode(code)} className={styles.btn}>
-      <Icon name="copy" color="grayLv3" />
-      <span className="a11y-hidden">복사</span>
-    </button>
+    <div className={styles.container}>
+      <button
+        type="button"
+        onClick={() => copyCode(code)}
+        className={styles.btn}
+      >
+        <Icon name="copy" color="grayLv3" />
+        <span className="a11y-hidden">복사</span>
+      </button>
+      {showNotification && <div className={styles.tooltip}>복사 완료</div>}
+    </div>
   );
 }
