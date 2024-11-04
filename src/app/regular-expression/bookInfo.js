8b222cc167b1e5d2965b37d9ed6bb9427b3a632b
paullabkorea <paullabkorea@gmail.com>
2024-11-04 12:38:30 +0900

    정규표현식 책 업데이트

---
+++ src/app/regular-expression/bookInfo.js
@@ -0,0 +1,9 @@
+export const DEFAULT_PATH = 'regular-expression';
+export const TITLE = '정규표현식 톺아보기 with JS, Py';
+export const DESCRIPTION = '위니브의 다양한 교안을 웹에서 확인해보세요!';
+
+export const COVER = `/images/${DEFAULT_PATH}/cover.png`;
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover.png`;
+
+// 'HTML/CSS', 'Python', 'JavaScript' 중 선택하여 삽입해주세요
+export const EDITOR = [];
