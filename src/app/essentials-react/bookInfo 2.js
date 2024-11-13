9e6f62d9b55acb1c500715534f22520ea55a52d4
Sunryeo <elma9700@gmail.com>
2024-11-13 09:52:57 +0900

    📝 Others: 최신 변경사항 커밋

---
+++ src/app/essentials-react/bookInfo 2.js
@@ -0,0 +1,9 @@
+export const DEFAULT_PATH = 'essentials-react';
+export const TITLE = 'React 에센셜';
+export const DESCRIPTION = '위니브의 다양한 교안을 웹에서 확인해보세요!';
+
+export const COVER = `/images/${DEFAULT_PATH}/cover-essentials-react.png`;
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover-essentials-react.png`;
+
+// 'HTML/CSS', 'Python', 'JavaScript' 중 선택하여 삽입해주세요
+export const EDITOR = [];
---
+++ src/app/essentials-sql/bookInfo 2.js
@@ -0,0 +1,9 @@
+export const DEFAULT_PATH = 'essentials-sql';
+export const TITLE = 'SQL 베이스캠프';
+export const DESCRIPTION = '위니브의 다양한 교안을 웹에서 확인해보세요!';
+
+export const COVER = `/images/${DEFAULT_PATH}/cover-sql.png`;
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover-sql.png`;
+
+// 'HTML/CSS', 'Python', 'JavaScript' 중 선택하여 삽입해주세요
+export const EDITOR = [];
