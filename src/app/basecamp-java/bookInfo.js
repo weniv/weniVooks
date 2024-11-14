040ea7f88e6e71a45817a0e2cbc6da597eeb0907
unknown <elwl5515@gmail.com>
2024-11-14 17:56:57 +0900

    📝 Add: basecamp-java 추가

---
+++ src/app/basecamp-java/bookInfo.js
@@ -0,0 +1,10 @@
+export const DEFAULT_PATH = 'basecamp-java';
+export const TITLE = 'Java 베이스캠프';
+export const DESCRIPTION = '누구나 쉽게 시작하는 Java 첫걸음 가이드 Java 베이스캠프!';
+
+// TODO: 책 표지 이미지 교체 필요
+export const COVER = `/images/${DEFAULT_PATH}/cover.png`; // 책 표지
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover.png`; // 링크 공유 시 나올 이미지
+
+// 'HTML/CSS', 'Python', 'JavaScript' 중 선택하여 삽입해주세요
+export const EDITOR = ['HTML/CSS'];
