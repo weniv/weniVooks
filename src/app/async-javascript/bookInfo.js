57359264e13582128f2b747d1eb599631d98023a
gyeongbaek <gyeongbaek110@gmail.com>
2024-11-18 16:24:31 +0900

    :memo: Edit: 떠먹는 비동기 자바스크립트 강의 교안 업로드

---
+++ src/app/async-javascript/bookInfo.js
@@ -0,0 +1,10 @@
+export const DEFAULT_PATH = 'async-javascript';
+export const TITLE = '떠먹는 자바스크립트 비동기';
+export const DESCRIPTION =
+  '자바스크립트 비동기 동작원리부터 비동기 제어, 통신까지 전반을 알아보세요!';
+
+export const COVER = `/images/${DEFAULT_PATH}/cover.png`; // 책 표지
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover.png`; // 링크 공유 시 나올 이미지
+
+// 'HTML/CSS', 'Python', 'JavaScript' 중 선택하여 삽입해주세요
+export const EDITOR = ['JavaScript'];
