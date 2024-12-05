60cb2f16c40e7f28ca711509ba615ed20d6d1b9d
paullabkorea <paullabkorea@gmail.com>
2024-12-05 17:26:23 +0900

    깃헙 베이스캠프와 노션 베이스캠프 페이지 업로드

---
+++ src/app/basecamp-notion/[chapter]/page.jsx
@@ -0,0 +1,18 @@
+import { DEFAULT_PATH, TITLE } from '../bookInfo';
+import { getChapters } from '@/sub/getChapters';
+import ChapterPage from '@/components/pages/ChapterPage';
+
+// 챕터 페이지
+export default function page({ params }) {
+  const chapter = params.chapter;
+
+  return (
+    <ChapterPage DEFAULT_PATH={DEFAULT_PATH} TITLE={TITLE} chapter={chapter} />
+  );
+}
+
+export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return getChapters(DEFAULT_PATH);
+}
---
+++ src/app/basecamp-notion/bookInfo.js
@@ -0,0 +1,9 @@
+export const DEFAULT_PATH = 'basecamp-notion';
+export const TITLE = 'Notion 베이스캠프';
+export const DESCRIPTION = '위니브의 다양한 교안을 웹에서 확인해보세요!';
+
+export const COVER = `/images/${DEFAULT_PATH}/cover.png`;
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover.png`;
+
+// 'HTML/CSS', 'Python', 'JavaScript' 중 선택하여 삽입해주세요
+export const EDITOR = [];
