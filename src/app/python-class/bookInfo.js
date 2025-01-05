58d2bfb56fd4450c33b489cf0db5a587c1c5988b
paullabkorea <paullabkorea@gmail.com>
2025-01-05 15:17:39 +0900

    파이썬 톺아보기 업로드

--- src/app/python-class/bookInfo.js
+++ src/app/python-class/bookInfo.js
@@ -5,5 +5,4 @@ export const DESCRIPTION = '위니브의 다양한 교안을 웹에서 확인해
 export const COVER = `/images/${DEFAULT_PATH}/cover.png`;
 export const OG_IMG = `/images/${DEFAULT_PATH}/cover.png`;
 
-// 'HTML/CSS', 'Python', 'JavaScript' 중 선택하여 삽입해주세요
-export const EDITOR = [];
+export const EDITOR = ['Python'];
