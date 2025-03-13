ccf2d199ee7336e8c06cdcec0a116aae874c432a
happydata1510 <happydata1510@gmail.com>
2025-03-14 07:54:40 +0900

    데이터베이스캠프 초기버전 재업데이트

---
+++ src/app/databasecamp-dataintro/bookInfo.js
@@ -0,0 +1,9 @@
+export const DEFAULT_PATH = 'databasecamp-dataintro';
+export const TITLE = '데이터베이스캠프 데이터Intro';
+export const DESCRIPTION = '위니브의 다양한 교안을 웹에서 확인해보세요!';
+
+export const COVER = `/images/${DEFAULT_PATH}/cover-sql.png`;
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover-sql.png`;
+
+// 'HTML/CSS', 'Python', 'JavaScript' 중 선택하여 삽입해주세요
+export const EDITOR = [];
