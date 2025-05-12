f614bc1170bd45ccc30182748a033cc720f9a09e
lynnjean <kimlynnjean@naver.com>
2025-05-12 16:31:08 +0900

    📝 Streamlit 파일명 변경

--- src/app/basecamp-streamlit/bookInfo.js
+++ src/app/basecamp-streamlit/bookInfo.js
@@ -1,6 +1,6 @@
-export const DEFAULT_PATH = 'streamlit';
-export const TITLE = 'Streamlit';
-export const DESCRIPTION = 'Streamlit 교안을 웹에서 확인해보세요!';
+export const DEFAULT_PATH = 'basecamp-streamlit';
+export const TITLE = 'Streamlit 베이스캠프';
+export const DESCRIPTION = 'Streamlit 베이스캠프 교안을 웹에서 확인해보세요!';
 
 export const COVER = `/images/${DEFAULT_PATH}/cover.png`; // 책 표지
 export const OG_IMG = `/images/${DEFAULT_PATH}/cover.png`; // 링크 공유 시 나올 이미지
