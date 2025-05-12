8c120bf04d5dfe22e1a8fc2acbf4a1463e31f388
lynnjean <kimlynnjean@naver.com>
2025-05-12 14:51:42 +0900

    ✨ Streamlit 도서 추가

---
+++ src/app/streamlit/bookInfo.js
@@ -0,0 +1,9 @@
+export const DEFAULT_PATH = 'streamlit';
+export const TITLE = 'Streamlit';
+export const DESCRIPTION = 'Streamlit 교안을 웹에서 확인해보세요!';
+
+export const COVER = `/images/${DEFAULT_PATH}/cover.png`; // 책 표지
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover.png`; // 링크 공유 시 나올 이미지
+
+// 'HTML/CSS', 'Python', 'JavaScript' 중 선택하여 삽입해주세요
+export const EDITOR = ['Python'];
