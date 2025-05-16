3a7d61f2834cc8a6b625355b73e2bd6dd37efd6c
chimaek <pipiru100@gmail.com>
2025-05-16 15:21:46 +0900

    경로 치환

--- next.config.js
+++ next.config.js
@@ -4,6 +4,19 @@ const nextConfig = {
   assetPrefix: '/services/wenivooks',
   trailingSlash: true,
   output: 'standalone', // Docker standalone 빌드
+
+  async rewrites() {
+    return [
+      {
+        source: '/images/:path*',
+        destination: '/services/wenivooks/images/:path*',
+      },
+      {
+        source: '/pyscript/:path*',
+        destination: '/services/wenivooks/pyscript/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
