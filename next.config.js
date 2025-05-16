f8b96e0c95c01bd4f87e0cefd61c214c1bef9a85
chimaek <pipiru100@gmail.com>
2025-05-16 13:33:54 +0900

    dev 환경 추가 (브랜치)

--- next.config.js
+++ next.config.js
@@ -1,20 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/services/wenivooks',
-  assetPrefix: '/services/wenivooks',
+  // assetPrefix: '/services/wenivooks',
   trailingSlash: true,
   output: 'standalone', // Docker standalone 빌드
 
   // 정적 파일 서빙 설정 추가
+  // 정적 파일 경로 재작성
   async rewrites() {
     return [
       {
-        source: '/services/wenivooks/images/:path*',
-        destination: '/images/:path*',
+        source: '/images/:path*',
+        destination: '/services/wenivooks/images/:path*',
       },
       {
-        source: '/services/wenivooks/pyscript/:path*',
-        destination: '/pyscript/:path*',
+        source: '/pyscript/:path*',
+        destination: '/services/wenivooks/pyscript/:path*',
       },
     ];
   },
@@ -22,7 +23,6 @@ const nextConfig = {
   // 이미지 설정
   images: {
     unoptimized: true,
-    domains: ['dev.wenivops.co.kr'],
   },
 };
 
