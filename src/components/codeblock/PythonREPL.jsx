75a3095be7ab28b3dcc1d6f0aa2b4f99512a4d65
chimaek <pipiru100@gmail.com>
2025-05-16 15:15:03 +0900

    PythonREPL.jsx 수정

--- src/components/codeblock/PythonREPL.jsx
+++ src/components/codeblock/PythonREPL.jsx
@@ -2,24 +2,20 @@ import React from 'react';
 import Script from 'next/script';
 
 export default function PythonREPL({ children }) {
-  const basePath = process.env.NODE_ENV === 'dev' ? '/services/wenivooks' : '';
-  console.log(basePath);
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
   return (
     <div>
-      {/* PyScript CSS */}
       <link
         rel="stylesheet"
         href={`${basePath}/pyscript/pyscript_init.css`}
       />
-
-      {/* PyScript JS - Script 태그에 strategy 추가 */}
       <Script
         src={`${basePath}/pyscript/pyscript_init.js`}
         strategy="beforeInteractive"
         onLoad={() => console.log('PyScript loaded')}
-        onError={() => console.error('PyScript failed to load')}
+        onError={(e) => console.error('PyScript failed to load:', e)}
       />
-
       <py-repl>{children}</py-repl>
     </div>
   );
