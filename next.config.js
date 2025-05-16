b69117a1c08d22c5b7bd8034b3466b29bc7b8ccc
chimaek <pipiru100@gmail.com>
2025-05-16 12:06:45 +0900

    dev 환경 추가 (브랜치)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/services/wenivooks',
+  assetPrefix: '/services/wenivooks',
+  trailingSlash: true,
+  output: 'standalone', // Docker용 standalone 빌드
+  experimental: {
+    // 필요시 추가 설정
+  }
+};
 
 module.exports = nextConfig;
